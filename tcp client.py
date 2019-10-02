06f8d1a11f564fa14de28eb64546b5072b335b9d
burakuccc <56093890+burakuccc@users.noreply.github.com>
2019-10-03 01:06:47 +0300

    Add files via upload

--- tcp client.py
+++ tcp client.py
@@ -1,7 +1,8 @@
 from socket import *
-
+host = input("Please enter the host: ")
+port = input("Please enter the port")
 s = socket(AF_INET,SOCK_STREAM)
-s.connect(('www.python.org',80))
+s.connect((host,port))
 s.sendall("GET /index.html HTTP/1.0\n\n".encode())
 data = s.recv(10000)
 print(data)
